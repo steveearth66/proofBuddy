d4b244c2f7d7b487c586acbc51d4472532893010
Steve Earth <se435@drexel.edu>
2022-08-11 14:42:33 -0400

    commenting out last line of settings

--- prooftool/settings.py
+++ prooftool/settings.py
@@ -177,4 +177,4 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals()) #this line was causing error
